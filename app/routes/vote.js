f2996abc68f7a6e7c712bababc9fc2bf3f33e1c7
Lobarr <jesuloba.egunjobi@uoit.net>
2017-06-05 17:39:35 -0400

    updated vote route to separate file

--- app/routes/vote.js
+++ app/routes/vote.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const PollModel = require('../database/models/polls')
 
-router.post('/vote/:id/:option', (req, res) => {
-
+router.post('/vote/:id', (req, res) => {
+  PollModel.vote(req.params.id, parseInt(req.body._vote), (err, poll) => {
+    if (err) throw err
+    console.log(poll.poll)
+    console.log(poll.poll.options[`${parseInt(req.body._vote)}`][1]);
+    req.flash('success', 'Successfully voted!');
+    res.redirect('/poll/'+req._parsedOriginalUrl.pathname.substr(6, req._parsedOriginalUrl.pathname.length))
+  })  
 })
 
 
